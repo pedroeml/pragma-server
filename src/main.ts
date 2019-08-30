991bd3799a4140e95c7fe6efd547dd46366bf548
Pedro Lemos <pedroemlemos@gmail.com>
2019-08-30 01:12:55 -0300

    Enabling CORS

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(compression());
   await app.listen(process.env.PORT || 3000);
 }
